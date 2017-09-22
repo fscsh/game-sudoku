79a55865b00324e88cc7adff4fd85f9eabe6c371
Shuhao Chen <shuhao.chen@sjsu.edu>
2017-09-21 22:57:43 -0700

    fix number

--- js/botton.js
+++ js/botton.js
@@ -1,6 +1,6 @@
-var Difficultylevel, board, answerBoardStr, displayval, chooseIdx, chooseAns, endtime = 0;
+var Difficultylevel, board, answerBoardStr, displayval, chooseIdx, chooseAns, level,endtime = 0;
 $(document).ready(function() {
-    // $(".level-container").hide();
+    $(".level-container").hide();
     $("#gameboard").hide();
     document.getElementById("easy").innerHTML = "EASY";
     document.getElementById("midium").innerHTML = "MIDIUM";
@@ -8,6 +8,7 @@ $(document).ready(function() {
 })
 $("#easy").click(function() {
     Difficultylevel = 10;
+    level = 10;
     board = [
         ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
@@ -39,17 +40,88 @@ $("#easy").click(function() {
     }
     $("#gameboard").show();
 })
+
+$("#midium").click(function() {
+    Difficultylevel = 20;
+    level = 20;
+    board = [
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.']
+    ];
+    solveBoardStr = isValidSudoku(board);
+    for (let i = 0; i < solveBoardStr.length; i++) {
+        solveBoardStr[i] = solveBoardStr[i].join('');
+    }
+    answerBoardStr = origintoStr(solveBoardStr);
+    let useBoard = [];
+    for (let i = 0; i < solveBoardStr.length; i++) {
+        let arr = [];
+        for (let j = 0; j < solveBoardStr[i].length; j++) {
+            arr.push(solveBoardStr[i][j])
+        }
+        useBoard.push(arr);
+    }
+    displayval = playboardStr(useBoard, Difficultylevel);
+
+    for (var i = 0; i < 81; i++) {
+        document.getElementById('c' + i).innerHTML = displayval[i];
+    }
+    $("#gameboard").show();
+})
+
+$("#hard").click(function() {
+    Difficultylevel = 50;
+    level = 50;
+    board = [
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.']
+    ];
+    solveBoardStr = isValidSudoku(board);
+    for (let i = 0; i < solveBoardStr.length; i++) {
+        solveBoardStr[i] = solveBoardStr[i].join('');
+    }
+    answerBoardStr = origintoStr(solveBoardStr);
+    let useBoard = [];
+    for (let i = 0; i < solveBoardStr.length; i++) {
+        let arr = [];
+        for (let j = 0; j < solveBoardStr[i].length; j++) {
+            arr.push(solveBoardStr[i][j])
+        }
+        useBoard.push(arr);
+    }
+    displayval = playboardStr(useBoard, Difficultylevel);
+
+    for (var i = 0; i < 81; i++) {
+        document.getElementById('c' + i).innerHTML = displayval[i];
+    }
+    $("#gameboard").show();
+})
+
 $('#answerchoice').hide();
 $('#gameboard').click(function(e) {
     if (e.target.innerHTML === '.') {
         // debugger
+
         let idName = e.target.id;
         $('#' + idName).css('background', 'green');
         chooseIdx = idName.substring(idName.lastIndexOf('c') + 1);
         chooseAns = answerBoardStr[chooseIdx];
         // debugger
-        endtime++;
-        winCheck();
+
         $('#answerchoice').show();
     } else {
         $('#answerchoice').hide();
@@ -57,25 +129,34 @@ $('#gameboard').click(function(e) {
     }
 })
 $('#answerchoice').click(function(e) {
+
     let idChoose = e.target.id;
     answerIdx = idChoose.substring(idChoose.lastIndexOf('a') + 1);
     if (answerIdx === chooseAns) {
-        console.log('correct!');
+        // console.log('correct!');
         document.getElementById('c' + chooseIdx).innerHTML = chooseAns;
+        endtime++;
+        winCheck();
         let optionCorrect = '#c' + chooseIdx;
         $(optionCorrect).css('background', 'white');
     } else {
         // document.getElementById('c'+chooseIdx).innerHTML = chooseAns;
         let optionName = '#c' + chooseIdx;
         $(optionName).css('background', 'red');
+        document.getElementById('c' + chooseIdx).innerHTML = answerIdx;
     }
 })
 
 $('.gameover').hide();
 
 function winCheck() {
-    if (endtime === 1) {
+    if (endtime === level) {
         $('.gameover').show();
         $('#gameboard').hide();
+        $('#answerchoice').hide();
+        $('start').show();
+    }else {
+        $('.gameover').hide();
+
     }
 }
